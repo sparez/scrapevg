c56194df7b6cfabde3dc45e4b38a2a89908263e3
Reza Marvan Spagnolo <marvans@gmail.com>
2012-08-15 12:51:41 +0200

    added version file in source. fixed failing test for cli parser tests

---
+++ lib/scrapevg/version.rb
@@ -0,0 +1,3 @@
+module Scrapevg
+  VERSION = '0.0.1'
+end
--- test/scrapevg/test_cli_parser.rb
+++ test/scrapevg/test_cli_parser.rb
@@ -3,8 +3,10 @@ require 'test/unit'
 class Scrapevg::CliParserTest < Test::Unit::TestCase
   def test_parse_args
     test_url = 'http://foo.bar'
-    test_argv = [ test_url ]
+    test_target = '/tmp/mysvgfiles'
+    test_argv = [ test_url, test_target ]
     options = Scrapevg::CliParser.parse_args(test_argv)
     assert_equal options[:url], test_url
+    assert_equal options[:target], test_target
   end
 end
