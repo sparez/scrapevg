2adeca5ccbea2fdd7a3cb9e64aff18dd1a5e3867
Reza Marvan Spagnolo <marvans@gmail.com>
2012-08-28 00:36:44 +0200

    Began using methadone for cli parsing. Removed CliParser class and related test case. Issue #3.

--- lib/scrapevg.rb
+++ lib/scrapevg.rb
@@ -9,26 +9,20 @@
 module Scrapevg
 
   # Run Scrapevg main logic
-  def self.run
-    begin
-      args = CliParser.parse_args(ARGV)
-      page = PageScraper.scrape_url( args[:url] )
-      svg_scraper = SvgScraper.new(page)
-      if svg_scraper.svg_elements.size > 0
-        puts "Found #{svg_scraper.svg_elements.size} SVG elements"
-        puts "Writing the file(s) to the specified target directory: #{args[:target]}"
-        svg_scraper.write_to_directory(args[:target])
-        puts 'Done!'
-      else
-        puts "No SVG elements found in given page #{args[:url]}"
-      end
-    rescue ArgumentError => ae
-      puts ae.message
+  def self.run(page_url,target_dir)
+    page = PageScraper.scrape_url( page_url )
+    svg_scraper = SvgScraper.new(page)
+    if svg_scraper.svg_elements.size > 0
+      puts "Found #{svg_scraper.svg_elements.size} SVG elements"
+      puts "Writing the file(s) to the specified target directory: #{target_dir}"
+      svg_scraper.write_to_directory(target_dir)
+      puts 'Done!'
+    else
+      puts "No SVG elements found in given page #{page_url}"
     end
   end
 
 end
 
-require 'scrapevg/cli_parser'
 require 'scrapevg/page_scraper'
 require 'scrapevg/svg_scraper'
