6d67060192d1c92e174d9db59026c534a08b14ac
Reza Marvan Spagnolo <marvans@gmail.com>
2012-08-29 00:48:50 +0200

    now using CLILogger from methadone. re-enabled rescueing of exception thrown by page scrape in case of invalid URL.

--- lib/scrapevg.rb
+++ lib/scrapevg.rb
@@ -2,6 +2,8 @@
 # Author:: Reza Marvan Spagnolo
 # License:: see LICENSE
 
+require 'methadone/cli_logger'
+
 #
 # Main Scrapevg driver
 # Launch with Scrapevg.run()
@@ -10,15 +12,20 @@ module Scrapevg
 
   # Run Scrapevg main logic
   def self.run(page_url,target_dir)
-    page = PageScraper.scrape_url( page_url )
-    svg_scraper = SvgScraper.new(page)
-    if svg_scraper.svg_elements.size > 0
-      puts "Found #{svg_scraper.svg_elements.size} SVG elements"
-      puts "Writing the file(s) to the specified target directory: #{target_dir}"
-      svg_scraper.write_to_directory(target_dir)
-      puts 'Done!'
-    else
-      puts "No SVG elements found in given page #{page_url}"
+    begin
+      logger = Methadone::CLILogger.new
+      page = PageScraper.scrape_url( page_url )
+      svg_scraper = SvgScraper.new(page)
+      if svg_scraper.svg_elements.size > 0
+        logger.info "Found #{svg_scraper.svg_elements.size} SVG elements"
+        logger.info "Writing the file(s) to the specified target directory: #{target_dir}"
+        svg_scraper.write_to_directory(target_dir)
+        logger.info 'Done!'
+      else
+        logger.warn "No SVG elements found in given page #{page_url}"
+      end
+    rescue ArgumentError => ae
+      logger.error ae.message
     end
   end
 
@@ -26,3 +33,4 @@ end
 
 require 'scrapevg/page_scraper'
 require 'scrapevg/svg_scraper'
+require 'scrapevg/version'
